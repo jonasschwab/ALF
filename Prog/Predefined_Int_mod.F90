8e418990dbf4d7139b1cff774fe84f48f1a79139
Anika Goetz <anika.goetz@stud-mail.uni-wuerzburg.de>
2022-08-10 00:45:00 +0200

    Time dependency in predefined interaction

--- Prog/Predefined_Int_mod.F90
+++ Prog/Predefined_Int_mod.F90
@@ -56,12 +56,13 @@
 !> U/N_{N_SUN}  [ \sum_{sigma=1}^{N_SUN}( n_{i,sigma} - 1/2 ) ]^2
 !> This is for an  SU(N) code. In this case the Hubbard U  is defined only by one operator.  
 !--------------------------------------------------------------------
-      Subroutine Predefined_Int_U_SUN( OP, I, N_SUN, DTAU, U  )
+      Subroutine Predefined_Int_U_SUN( OP, I, N_SUN, DTAU, U, gU_t  )
         
         Implicit none
         Integer,  Intent(In) :: I, N_SUN 
         Real (Kind=Kind(0.d0)), Intent(IN) :: Dtau, U
         Type(Operator), Intent(Out) :: OP
+        real (kind=kind(0.d0)), optional, intent(in) :: gU_t(:)
 
         Call OP_Make( Op,1 )
 
@@ -70,6 +71,10 @@
         Op%alpha  = cmplx(-0.5d0,0.d0, kind(0.D0))
         Op%g      = SQRT(CMPLX(-DTAU*U/(DBLE(N_SUN)), 0.D0, kind(0.D0))) 
         Op%type   = 2
+        if (present(gU_t)) then
+            allocate (Op%g_t(size(gU_t,1)))
+            Op%g_t = SQRT(CMPLX(-DTAU*U*gU_t/(DBLE(N_SUN)), 0.D0, kind(0.D0)))
+        endif
         
         Call Op_set( Op )
         
@@ -87,12 +92,13 @@
 !>   Here A^2 =  -\Dtau U/N_{N_SUN}  [ \sum_{sigma=1}^{N_SUN}( n_{i,sigma} - 1/2 ) ]^2 
 !> 
 !--------------------------------------------------------------------
-      Subroutine Predefined_Int_U_SUN_continuous_HS( OP, I, N_SUN, DTAU, U  )
+      Subroutine Predefined_Int_U_SUN_continuous_HS( OP, I, N_SUN, DTAU, U, gU_t  )
         
         Implicit none
         Integer,  Intent(In) :: I, N_SUN 
         Real (Kind=Kind(0.d0)), Intent(IN) :: Dtau, U
         Type(Operator), Intent(Out) :: OP
+        real (kind=kind(0.d0)), optional, intent(in) :: gU_t(:)
 
         Call OP_Make( Op,1 )
 
@@ -101,6 +107,10 @@
         Op%alpha  = cmplx(-0.5d0,0.d0, kind(0.D0))
         Op%g      = SQRT(CMPLX(-DTAU*U*2.d0/(DBLE(N_SUN)), 0.D0, kind(0.D0))) 
         Op%type   = 3
+        if (present(gU_t)) then
+            allocate (Op%g_t(size(gU_t,1)))
+            Op%g_t = SQRT(CMPLX(-DTAU*U*gU_t*2.d0/(DBLE(N_SUN)), 0.D0, kind(0.D0)))
+        endif
         
         Call Op_set( Op )
         
@@ -118,12 +128,13 @@
 !>   The routine uses a  descrete  HS transformation  with four fields
 !>   (see documentation)
 !--------------------------------------------------------------------
-      Subroutine Predefined_Int_U_MZ (  OP_up,  Op_do, I, DTAU, U  ) 
+      Subroutine Predefined_Int_U_MZ (  OP_up,  Op_do, I, DTAU, U, gU_t  )
 
         Implicit none
         Integer,  Intent(In) :: I
         Real (Kind=Kind(0.d0)), Intent(IN) ::  Dtau, U
         Type(Operator), Intent(Out) :: OP_up, Op_do
+        real (kind=kind(0.d0)), optional, intent(in) :: gU_t(:)
 
         Call OP_Make( Op_up,1 )
         Call OP_Make( Op_do,1 )
@@ -134,6 +145,10 @@
         Op_up%g      =  SQRT(CMPLX(DTAU*U/2.d0, 0.D0, kind(0.D0))) 
         Op_up%alpha  =  cmplx(-0.5d0,0.d0, kind(0.D0))
         Op_up%type   =  2
+        if (present(gU_t)) then
+            allocate (Op_up%g_t(size(gU_t,1)))
+            Op_up%g_t = SQRT(CMPLX(DTAU*U*gU_t/2.d0, 0.D0, kind(0.D0)))
+        endif
 
         Op_do%P(1)   =  I
         Op_do%O(1,1) =  cmplx(1.d0  ,0.d0, kind(0.D0))
@@ -141,6 +156,10 @@
         Op_do%g      = -SQRT(CMPLX(DTAU*U/2.d0, 0.D0, kind(0.D0))) 
         Op_do%alpha  =  cmplx(-0.5d0,0.d0, kind(0.D0))
         Op_do%type   =  2
+        if (present(gU_t)) then
+            allocate (Op_do%g_t(size(gU_t,1)))
+            Op_do%g_t = -SQRT(CMPLX(DTAU*U*gU_t/2.d0, 0.D0, kind(0.D0)))
+        endif
 
         Call Op_set( Op_up )
         Call Op_set( Op_do )
@@ -158,12 +177,13 @@
 !>   e^{A^2} = \int dx e^{ - x^2/2 + \sqrt{2} A x }
 !>   Here A^2 =  \Dtau U /2  ( n_{i,up} - n_{i,do} ) ^2  
 !--------------------------------------------------------------------
-      Subroutine Predefined_Int_U_MZ_continuous_HS(  OP_up,  Op_do, I, DTAU, U  ) 
+      Subroutine Predefined_Int_U_MZ_continuous_HS(  OP_up,  Op_do, I, DTAU, U, gU_t  )
 
         Implicit none
         Integer,  Intent(In) :: I
         Real (Kind=Kind(0.d0)), Intent(IN) ::  Dtau, U
         Type(Operator), Intent(Out) :: OP_up, Op_do
+        real (kind=kind(0.d0)), optional, intent(in) :: gU_t(:)
 
         Call OP_Make( Op_up,1 )
         Call OP_Make( Op_do,1 )
@@ -173,12 +193,20 @@
         Op_up%alpha  =  cmplx(0.d0, 0.d0, kind(0.D0))
         Op_up%g      =  SQRT(CMPLX(DTAU*U, 0.D0, kind(0.D0))) 
         Op_up%type   =  3
+        if (present(gU_t)) then
+            allocate (Op_up%g_t(size(gU_t,1)))
+            Op_up%g_t = SQRT(CMPLX(DTAU*U*gU_t, 0.D0, kind(0.D0)))
+        endif
 
         Op_do%P(1)   =  I
         Op_do%O(1,1) =  cmplx(1.d0  ,0.d0, kind(0.D0))
         Op_do%alpha  =  cmplx(0.d0, 0.d0, kind(0.D0))
         Op_do%g      = -SQRT(CMPLX(DTAU*U, 0.D0, kind(0.D0))) 
         Op_do%type   =  3
+        if (present(gU_t)) then
+            allocate (Op_do%g_t(size(gU_t,1)))
+            Op_do%g_t = -SQRT(CMPLX(DTAU*U*gU_t, 0.D0, kind(0.D0)))
+        endif
 
         Call Op_set( Op_up )
         Call Op_set( Op_do )
@@ -197,12 +225,13 @@
 !>   This is for an SU(N) symmetric code such that the above  interaction corresponds to just one operator
 !>   
 !--------------------------------------------------------------------
-      Subroutine Predefined_Int_V_SUN( OP, I, J, N_SUN, DTAU, V  ) 
+      Subroutine Predefined_Int_V_SUN( OP, I, J, N_SUN, DTAU, V, gV_t  )
         
         Implicit none
         Integer,  Intent(In) :: I, J, N_SUN
         Real (Kind=Kind(0.d0)), Intent(IN) ::  Dtau, V
         Type(Operator), Intent(Out) :: OP
+        real (kind=kind(0.d0)), optional, intent(in) :: gV_t(:)
 
         Call OP_Make( Op,2 )
 
@@ -213,6 +242,10 @@
         Op%g      = SQRT(CMPLX(DTAU*V/real(N_SUN,kind(0.d0)), 0.D0, kind(0.D0))) 
         Op%alpha  = cmplx(0.d0, 0.d0, kind(0.D0))
         Op%type   = 2
+        if (present(gV_t)) then
+            allocate (Op%g_t(size(gV_t,1)))
+            Op%g_t = SQRT(CMPLX(DTAU*V*gV_t/real(N_SUN,kind(0.d0)), 0.D0, kind(0.D0)))
+        endif
         
         Call Op_set( Op )
         
@@ -229,12 +262,13 @@
 !>   This is for an SU(N) symmetric code such that the above  interaction corresponds to just one operator
 !>   
 !--------------------------------------------------------------------
-      Subroutine Predefined_Int_VJ_SUN( OP, I, J, N_SUN, DTAU, V  ) 
+      Subroutine Predefined_Int_VJ_SUN( OP, I, J, N_SUN, DTAU, V, gV_t  )
         
         Implicit none
         Integer,  Intent(In) :: I, J, N_SUN
         Real (Kind=Kind(0.d0)), Intent(IN) ::  Dtau, V
         Type(Operator), Intent(Out) :: OP
+        real (kind=kind(0.d0)), optional, intent(in) :: gV_t(:)
 
         Call OP_Make( Op,2 )
 
@@ -245,6 +279,10 @@
         Op%g      = SQRT(CMPLX(DTAU*V/real(N_SUN,kind(0.d0)), 0.D0, kind(0.D0))) 
         Op%alpha  = cmplx(0.d0, 0.d0, kind(0.D0))
         Op%type   = 2
+        if (present(gV_t)) then
+            allocate (Op%g_t(size(gV_t,1)))
+            Op%g_t = SQRT(CMPLX(DTAU*V*gV_t/real(N_SUN,kind(0.d0)), 0.D0, kind(0.D0)))
+        endif
         
         Call Op_set( Op )
         
@@ -261,12 +299,13 @@
 !>   This is for an SU(N) symmetric code, such that the above corresponds to one operator. 
 !>   
 !--------------------------------------------------------------------
-      Subroutine Predefined_Int_Ising_SUN( OP, I, J, DTAU, Xi  ) 
+      Subroutine Predefined_Int_Ising_SUN( OP, I, J, DTAU, Xi, gXi_t  )
         
         Implicit none
         Integer,  Intent(In) :: I, J
         Real (Kind=Kind(0.d0)), Intent(IN) ::  Dtau, Xi
         Type(Operator), Intent(Out) :: OP
+        real (kind=kind(0.d0)), optional, intent(in) :: gXi_t(:)
 
         Call OP_Make( Op,2 )
 
@@ -277,6 +316,10 @@
         Op%g      = cmplx(-dtau*xi,0.D0,kind(0.D0))
         Op%alpha  = cmplx(0d0,0.d0, kind(0.D0)) 
         Op%type   = 1
+        if (present(gXi_t)) then
+            allocate (Op%g_t(size(gXi_t,1)))
+            Op%g_t = cmplx(-dtau*xi*gxi_t,0.D0,kind(0.D0))
+        endif
         
         Call Op_set( Op )
         
@@ -325,12 +368,13 @@
 !>   If  particle fluctuations are frozen on the i and j sites then (S^{z}_i)^2 = 1/4
 !>   and the interactions corresponds to a Jz-Jz ferro or antiferro coupling.
 !--------------------------------------------------------------------
-      Subroutine Predefined_Int_Jz (  OP_up,  Op_do, I, J,  DTAU, Jz  ) 
+      Subroutine Predefined_Int_Jz (  OP_up,  Op_do, I, J,  DTAU, Jz, gJz_t  )
 
         Implicit none
         Integer,  Intent(In) :: I,J
         Real (Kind=Kind(0.d0)), Intent(IN) ::  Dtau, Jz
         Type(Operator), Intent(Out) :: OP_up, Op_do
+        real (kind=kind(0.d0)), optional, intent(in) :: gJz_t(:)
 
         Call OP_Make( Op_up,2 )
         Call OP_Make( Op_do,2 )
@@ -342,6 +386,10 @@
         Op_up%alpha  =  cmplx(0.d0, 0.d0, kind(0.D0))
         Op_up%g      =  SQRT(CMPLX(DTAU*Jz/8.d0, 0.D0, kind(0.D0))) 
         Op_up%type   =  2
+        if (present(gJz_t)) then
+            allocate (Op_up%g_t(size(gJz_t,1)))
+            Op_up%g_t = SQRT(CMPLX(DTAU*Jz*gJz_t/8.d0, 0.D0, kind(0.D0)))
+        endif
 
         Op_do%P(1)   =  I
         Op_do%P(2)   =  J
@@ -350,6 +398,10 @@
         Op_do%alpha  =  cmplx(0.d0, 0.d0, kind(0.D0))
         Op_do%g      = -SQRT(CMPLX(DTAU*Jz/8.d0, 0.D0, kind(0.D0))) 
         Op_do%type   =  2
+        if (present(gJz_t)) then
+            allocate (Op_do%g_t(size(gJz_t,1)))
+            Op_do%g_t = -SQRT(CMPLX(DTAU*JZ*gJz_t/8.d0, 0.D0, kind(0.D0)))
+        endif
 
         Call Op_set( Op_up ) 
         Call Op_set( Op_do )
