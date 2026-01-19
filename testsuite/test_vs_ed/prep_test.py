9d83c7af51f2a18a76b89a3570e8e2e798dc2c16
Jonas Schwab <jonas.schwab@uni-wuerzburg.de>
2026-01-19 11:03:25 +0100

    Work on .github/workflows/test-vs-ed.yaml

--- testsuite/test_vs_ed/prep_test.py
+++ testsuite/test_vs_ed/prep_test.py
@@ -3,7 +3,6 @@ import json
 import os
 import sys
 import yaml
-import copy
 
 if __name__ == "__main__":
     try:
@@ -13,13 +12,11 @@ if __name__ == "__main__":
     with open(specs_file, 'r', encoding='UTF-8') as f:
         test_specs = yaml.safe_load(f)
     
-    compile_matrix = []
     simulation_matrix = []
-    analysis_matrix = []
 
     for test_name, test_spec in test_specs.items():
         for env_name, env_spec in test_spec['environments'].items():
-            test_dir = f"testsuite/test_vs_ed/{test_name}_{env_name}"
+            test_dir = f"prepared-test-directories/{test_name}_{env_name}"
             os.makedirs(test_dir)
             with open(os.path.join(test_dir, "spec.yaml"), 'w', encoding='UTF-8') as f:
                 f.write(yaml.dump({
@@ -27,12 +24,6 @@ if __name__ == "__main__":
                     'env_name': env_name,
                     **test_spec,
                     }))
-            compile_matrix.append({
-                'test_name': test_name,
-                'env_name': env_name,
-                'machine': env_spec['variables']['MACHINE'],
-                'image': env_spec['image'],
-            })
             for i, _ in enumerate(test_spec["sim_dicts"]):
                 os.makedirs(os.path.join(test_dir, f"{i+1}"))
                 with open(os.path.join(test_dir, f"{i+1}", "spec.yaml"), 'w', encoding='UTF-8') as f:
@@ -50,5 +41,4 @@ if __name__ == "__main__":
                 })
     
     with open(os.getenv('GITHUB_OUTPUT', 'test.json'), 'w+', encoding='UTF-8') as f:
-      f.write(f'compile_matrix={json.dumps(compile_matrix)}\n')
       f.write(f'simulation_matrix={json.dumps(simulation_matrix)}\n')
